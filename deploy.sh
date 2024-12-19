52d9f6aaefee9d11b77b3edc8e48ab9853dfbd05
chpark <chpark@witchcompany.io>
2024-12-19 17:19:34 +0900

    deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,26 @@ ECR_URL_PATH="/home/ec2-user/app/ecr-url.txt"
 IMAGE_NAME=$(cat "$IMAGE_FILE_PATH") #image.txt에 저장한 도커이미지 정보
 ECR_URL=$(cat "$ECR_URL_PATH") #ecr-url.txt에 저장한 ECR URL 정보
 
-CURRENT_PID=$(docker container ls -q)
+# 현재 실행 중인 컨테이너 ID들 중 해당 이미지로 구동된 컨테이너 찾기
+CURRENT_PID=$(docker ps -q --filter "ancestor=$IMAGE_NAME")
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 구동중인 Docker Container가 없습니다"
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 $IMAGE_NAME 이미지로 구동 중인 Docker Container가 없습니다"
 else
-  echo "> docker stop $CURRENT_PID"
+  echo "> 실행 중인 Docker 컨테이너 삭제"
   sudo docker stop $CURRENT_PID
+
+  echo "> 기존 컨테이너 정지"
+  sudo docker rm $CURRENT_PID
+
+  echo "> 기존 Docker 이미지 삭제"
+  sudo docker rmi $IMAGE_NAME
+
+  echo "> Docker에서 사용하지 않는 자원 삭제"
+  docker system prune -af
   sleep 4
 fi
+
 
 echo "> login to ECR"
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin $ECR_URL
