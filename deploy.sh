0ea31f1f2d1b0a7dc41e694d8a5108f6df4dafb9
chpark <chpark@witchcompany.io>
2024-12-19 15:36:54 +0900

    ecr-url 추가

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 IMAGE_FILE_PATH="/home/ec2-user/app/image.txt"
+ERC_URL_PATH="/home/ec2-user/app/erc_url.txt"
 IMAGE_NAME=$(cat "$IMAGE_FILE_PATH") #image.txt에 저장한 도커이미지 정보
+ERC_URL=$(cat "$ERC_URL_PATH") #erc_url.txt에 저장한 도커이미지 정보
 
 CURRENT_PID=$(docker container ls -q)
 
-if [-z $CURRENT_PID]
+if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 Docker Container가 없습니다"
 else
@@ -14,7 +16,7 @@ else
 fi
 
 echo "> login to ECR"
-aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin [ECR_REGISTRY__NAME]
+aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin $ERC_URL
 
 echo "> docker pull $IMAGE_NAME"
 docker pull $IMAGE_NAME
